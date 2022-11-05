b038559e702d8bb1a15783f84cb31aabcab3a551
nike-tombasov2 <nike.tombasov@ya.ru>
2022-11-05 22:03:26 +0300

    Homework4 started

---
+++ HW4/Task1.py
@@ -0,0 +1,12 @@
+print("Homework 4. Task 1. Round up inserted number to the power of", 
+        "another inserted number formed as 10 ** (-1) <= 1 <= 10 ** (-10)")
+print()
+
+d = int(input("Enter your dot number in range 10 ** (-1) <= 1 <= 10 ** (-10): "))
+# d = 0.001
+num = input("Enter your decimal float number: ")
+# num = 123.456789123456789
+
+
+print(len(str(d).split(".")[1]))
+# k = len(num.split(".")[1])
