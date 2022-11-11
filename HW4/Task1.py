6af3f1c341d155bc15b6f35ca66b691caf9ae2f5
Nike Tombasov <nike.tombasov@ya.ru>
2022-11-11 19:14:56 +0300

    Homework 4 completed

--- HW4/Task1.py
+++ HW4/Task1.py
@@ -1,12 +1,12 @@
 print("Homework 4. Task 1. Option 1. Round up float number to the power of", 
-        "inserted number formed as 10 ** (-1) <= 1 <= 10 ** (-10)")
+        "inserted number formed as [10 ** (-1) <= d <= 10 ** (-10)]")
 print()
 
 from random import uniform
 from decimal import Decimal
 
-# d = input("Enter your dot number in range 10 ** (-1) <= 1 <= 10 ** (-10): ")
-d = Decimal("0.001")
+d = input("Enter your dot number in range 10 ** (-1) <= d <= 10 ** (-10): ")
+# d = Decimal("0.001")
 num = uniform(0, 100)
 
 print("Float number to round up:", num)
--- HW4/Task2.py
+++ HW4/Task2.py
@@ -2,8 +2,16 @@ print("Homework 4. Task 2. Make list of simple multipliers",
         "of inserted number.")
 print()
 
-num = 13
+# num = int(input("Enter your number: "))
+num = 1084
+temp, multipl, multipl_lst = num, 2, []
 
-# любое составное число имеет множитель, 
-# который не превосходит корень
-# этого числа
+while multipl <= temp:
+    if temp % multipl == 0:
+        multipl_lst.append(multipl)
+        temp //= multipl
+        multipl = 2
+    else: 
+        multipl += 1
+
+print(multipl_lst)
