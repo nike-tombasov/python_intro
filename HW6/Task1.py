404be33d326d89ab3b4e7abc0f60a643b09c72d8
Nike Tombasov <nike.tombasov@ya.ru>
2022-11-13 17:50:56 +0300

    Homework 6 started task 1

---
+++ HW6/Task1.py
@@ -0,0 +1,12 @@
+print("""Homework 6. Task 1. Make calculator that takes string of input data.
+        Use +, -, *, / operations only with logical priority. 
+        Additionaly include () operations with appropriate logical priority""", end="\n\n")
+
+#  2+2 => 4; 1+2*3 => 7; 1-2*3 => -5;
+#  1+2*3 => 7; (1+2)*3 => 9;
+
+expression = input("Enter your expression: ")
+
+
+
+print()
