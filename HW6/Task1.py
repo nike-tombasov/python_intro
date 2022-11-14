a46ec7560f1e42095dddb948519cd502e8055f2a
nike-tombasov2 <nike.tombasov@ya.ru>
2022-11-14 23:35:32 +0300

    Homework 6 task 1 remake

--- HW6/Task1.py
+++ HW6/Task1.py
@@ -2,11 +2,61 @@ print("""Homework 6. Task 1. Make calculator that takes string of input data.
         Use +, -, *, / operations only with logical priority. 
         Additionaly include () operations with appropriate logical priority""", end="\n\n")
 
+
 #  2+2 => 4; 1+2*3 => 7; 1-2*3 => -5;
 #  1+2*3 => 7; (1+2)*3 => 9;
 
-expression = input("Enter your expression: ")
+#expression = input("Enter your expression: ")
+expression = '5+25/5-2*2'
+plus_lst = expression.split('+')
+sum = 0
+for i in range(len(plus_lst)): # Main loop
+    if '-' in str(plus_lst[i]): # Minuses check and loop
+        minus_lst = plus_lst[i].split('-')
+        minus = 0
+        for j in range(len(minus_lst)):
+            if '*' in str(minus_lst[j]): # Multipl check and loop
+                mult_lst = minus_lst[j].split('*')
+                mult = 1
+                for k in range(len(mult_lst)):
+                    if '/' in str(mult_lst[k]): # Divid check and loop
+                        divid_lst = mult_lst[k].split('/')
+                        for l in range(len(divid_lst)-1):
+                            if int(divid_lst[l+1]) == 0:
+                                print("Division by zero. Wrong expression.")
+                            divid = int(divid_lst[l]) / int(divid_lst[l+1])
+                        mult_lst[k] = divid
+                    mult *= int(mult_lst[k])
+                minus_lst[j] = mult
+            if '/' in str(minus_lst[j]): # Divid check and loop
+                divid_lst = minus_lst[j].split('/')
+                for l in range(len(divid_lst)-1):
+                    if int(divid_lst[l+1]) == 0:
+                        print("Division by zero. Wrong expression.")
+                    divid = int(divid_lst[l]) / int(divid_lst[l+1])
+                minus_lst[j] = divid
+            minus -= int(minus_lst[j])
+        plus_lst[i] = minus
+    if '*' in str(plus_lst[i]): # Multipl check and loop
+        mult_lst = plus_lst[i].split('*')
+        mult = 1
+        for k in range(len(mult_lst)):
+            if '/' in mult_lst[k]:
+                divid_lst = mult_lst[k].split('/')
+                for l in range(len(divid_lst)-1):
+                    if int(divid_lst[l+1]) == 0:
+                        print("Division by zero. Wrong expression.")
+                    divid = int(divid_lst[l]) / int(divid_lst[l+1])
+            mult *= int(mult_lst[k])
+        plus_lst[i] = mult
+    if '/' in str(plus_lst[i]): # Multipl check and loop
+        divid_lst = plus_lst[i].split('/')
+        for l in range(len(divid_lst)-1):
+            if int(divid_lst[l+1]) == 0:
+                print("Division by zero. Wrong expression.")
+            divid = int(divid_lst[l]) / int(divid_lst[l+1])
+        plus_lst[i] = divid
+    sum += int(plus_lst[i])
+    print(i, " => ", sum)
 
-
-
-print()
+print(sum)
--- HW6/Task2.py
+++ HW6/Task2.py
@@ -1,20 +1,23 @@
 print("""Homework 6. Task 2. From given list return list with unique elements,
         list with doubling elements, with list without duplicates""", end="\n\n")
 
-# lst = [1, 2, 3, 5, 1, 5, 3, 10]
-lst = [5, 5 , 12, 0, 1, 12, 6, 5, 3]
-unique_lst = []
-doubling_lst = []
+import time
+
+start = time.time()
+
+lst = [1, 2, 3, 5, 1, 5, 3, 10, 5, 5, 12, 0, 1, 12, 6, 5, 3]
+unique_lst = list(i for i in set(lst) if lst.count(i) == 1)
+doubling_lst = list(set(lst) - set(unique_lst))
 clean_lst = list(set(lst))
 
-for i in set(lst):
-    count = lst.count(i)
-    if count == 1:
-        unique_lst.append(i)
-    elif count >= 2:
-        doubling_lst.append(i)
+# for i in set(lst):
+#     if lst.count(i) == 1:
+#         unique_lst.append(i)
+#     else:
+#         doubling_lst.append(i)
 
 print("Given list of element:", lst)
 print("List of unique elements from given list:", unique_lst)
 print("List of doubling elements from given list:", doubling_lst)
 print("List without duplicates:", clean_lst)
+print("Spent time:", (time.time() - start) * 1000)
